2b14e0b864a0384c9ff04c3170784d2d014439ff
Muhammad Rihap Firdaus <67884833+rihapfirdaus@users.noreply.github.com>
2024-12-12 11:47:11 +0700

    add domain for image

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["103.55.33.57"], // Add your image host here
+    domains: ["103.55.33.57","34.29.113.32"], // Add your image host here
   },
 };
 
